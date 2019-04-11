548566c1cc753a8b3502fcfe00d1da3af73f5d8c
Kernelgarden <kernelgarden@gmail.com>
2019-04-11 17:56:53 +0900

    add authserver user test

--- auth_server/lib/auth_server/model/user.ex
+++ auth_server/lib/auth_server/model/user.ex
@@ -2,10 +2,11 @@ defmodule AuthServer.Model.User do
   use Ecto.Schema
 
   alias __MODULE__
+  alias AuthServer.Repo
 
   import Ecto.Changeset
 
-  schema "people" do
+  schema "users" do
     field(:name, :string)
     field(:user_id, :string)
     field(:hashed_password, :string)
@@ -15,10 +16,18 @@ defmodule AuthServer.Model.User do
 
   def changeset(%User{} = user, attrs) do
     user
-    |> cast(attrs, [:player_name, :player_id, :password])
-    |> validate_required([:player_name, :player_id, :password])
-    |> unique_constraint(:player_id)
+    |> cast(attrs, [:user_id, :password, :name])
+    |> validate_required([:user_id, :password, :name])
+    |> unique_constraint(:user_id)
     |> put_hashed_password()
+  end
+
+  def get(user_id) do
+    Repo.get_by(User, user_id: user_id)
+  end
+
+  def authenticate(nil, _password)  do
+    false
   end
 
   def authenticate(%User{hashed_password: hashed_password}, password) do
--- auth_server/test/support/user_test.exs
+++ auth_server/test/support/user_test.exs
@@ -4,16 +4,83 @@ defmodule AuthServer.UserTest do
   import Ecto
   import Ecto.Query
 
+  alias AuthServer.Repo
+  alias AuthServer.Model.User
+
   setup do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(AuthServer.Repo)
+
+    %User{}
+      |> User.changeset(%{user_id: "test_id", password: "password", name: "testman"})
+      |> Repo.insert()
+
+    user = Repo.get_by(User, user_id: "test_id")
+
+    {:ok, user: user}
   end
 
-  test "create user" do
+  test "create user", _context do
+    new_user = %{user_id: "test_id2", password: "password", name: "testman"}
+
+    result =
+      %User{}
+      |> User.changeset(new_user)
+      |> Repo.insert()
+
+    assert elem(result, 0) == :ok
+
+    user = elem(result, 1)
+
+    assert user.user_id == new_user.user_id
+    assert user.password == new_user.password
+    assert user.name == new_user.name
+
+    stored_user = Repo.get_by(User, user_id: new_user.user_id)
+
+    assert stored_user != nil
+    assert stored_user.password == nil
   end
 
-  test "auth user" do
+  test "duplicate check test", context do
+    user = context.user
+    dummy_user = %{user_id: user.user_id, password: user.password, name: user.name}
+
+    result =
+      %User{}
+      |> User.changeset(dummy_user)
+      |> Repo.insert()
+
+    assert elem(result, 0) == :error
   end
 
-  test "delete user" do
+  test "auth user", context do
+    # 정상 요청 케이스
+    request = %{user_id: "test_id", password: "password"}
+
+    result =
+      User.get(request.user_id)
+      |> User.authenticate(request.password)
+
+    assert result == true
+
+    # 패스워드가 틀린 케이스
+    fake_request = %{user_id: "test_id", password: "passwor"}
+
+    result =
+      User.get(fake_request.user_id)
+      |> User.authenticate(fake_request.password)
+
+    assert result == false
+
+    # id가 틀린 케이스
+    fake_request2 = %{user_id: "test_id11", password: "password"}
+
+    result =
+      User.get(fake_request2.user_id)
+      |> User.authenticate(fake_request2.password)
+
+    assert result == false
+
   end
+
 end
