e1dafe5d3ae2af639fd4fe4a17f262c88fed09e5
Kernelgarden <kernelgarden@gmail.com>
2019-04-12 15:50:49 +0900

    change protocol num

--- auth_server/lib/auth_server/protocol.ex
+++ auth_server/lib/auth_server/protocol.ex
@@ -4,11 +4,11 @@ defmodule AuthServer.Protocol do
   # define packet type and packet number (please type full name)
   defenum Types do
     # sign up
-    value(AuthServer.Scheme.CS_Signup, 1)
-    value(AuthServer.Scheme.SC_Signup, 2)
+    value(AuthServer.Scheme.CS_Signup, 7)
+    value(AuthServer.Scheme.SC_Signup, 8)
 
     # sign in
-    value(AuthServer.Scheme.CS_Signin, 3)
-    value(AuthServer.Scheme.SC_Signin, 4)
+    value(AuthServer.Scheme.CS_Signin, 9)
+    value(AuthServer.Scheme.SC_Signin, 10)
   end
 end
